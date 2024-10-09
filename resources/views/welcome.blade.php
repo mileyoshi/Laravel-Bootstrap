10f4215eea70aa1e150638c77387478c70ffbfc4
Meli <meliherawati307@gmail.com>
2024-10-09 15:23:21 +0700

    add file

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Bootstrap</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js']);
+</head>
+<body>
+    <h1 class="text-danger">Welcome To Laravel</h1>
+</body>
+</html>
